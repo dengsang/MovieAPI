a1b842af6bda3953ea70c0b3d9984f44e536dd81
dengsang <dengsang@gmail.com>
2019-05-20 11:01:23 +0300

    parsing the jwt user authentication

--- src/main/java/com/movie/movie/security/exception/JwtParseException.java
+++ src/main/java/com/movie/movie/security/exception/JwtParseException.java
@@ -3,9 +3,9 @@ package com.movie.movie.security.exception;
 
 import org.springframework.security.core.AuthenticationException;
 
-public class JwtParserException extends AuthenticationException {
+public class JwtParseException extends AuthenticationException {
 
-    
+
     public JwtParseException(Exception e) {
         super(e.getMessage(), e);
     }
