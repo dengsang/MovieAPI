0338986fa63d98b4b56686fe95a1330f5c98ddb6
dengsang <dengsang@gmail.com>
2019-05-20 10:41:11 +0300

    adding the user implementation details

--- src/main/java/com/movie/movie/user/UserDetailsServiceImpl.java
+++ src/main/java/com/movie/movie/user/UserDetailsServiceImpl.java
@@ -1,4 +1,21 @@
 package com.movie.movie.user;
 
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserDetailsServiceImpl {
+
+    private final UserRepository userRepository;
+
+    //@Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return ( UserDetails ) userRepository.findByLogin(username)
+                .map(user -> new AuthenticatedUser(user.getLogin(), user.getPassword()))
+                .orElseThrow(() -> new UsernameNotFoundException("User is not found"));
+    }
 }
