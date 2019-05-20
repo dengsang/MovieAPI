78d912f19f92a9514d4dbdad398be9faf645267f
dengsang <dengsang@gmail.com>
2019-05-20 10:32:10 +0300

    modification to the UserService  java file

--- src/main/java/com/movie/movie/user/UserService.java
+++ src/main/java/com/movie/movie/user/UserService.java
@@ -1,4 +1,20 @@
 package com.movie.movie.user;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    public void removeAccount() {
+        String login = SecurityContextHolder.getContext().getAuthentication().getName();
+        userRepository.deleteByLogin(login);
+    }
 }
