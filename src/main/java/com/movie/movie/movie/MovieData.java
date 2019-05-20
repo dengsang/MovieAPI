a61fdedb0c246ffed13b4d492e6566dbacd6352f
dengsang <dengsang@gmail.com>
2019-05-20 11:05:47 +0300

    adding the movie data file with the setter settings

--- src/main/java/com/movie/movie/movie/MovieData.java
+++ src/main/java/com/movie/movie/movie/MovieData.java
@@ -2,12 +2,14 @@ package com.movie.movie.movie;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
+import org.springframework.stereotype.Component;
 
 import javax.validation.constraints.NotNull;
 
 
 @Data
-public class MovieDto {
+@Component
+public class MovieData extends Movie {
     private String id;
 
     @NotNull
@@ -18,9 +20,12 @@ public class MovieDto {
     @Length(min = 1, max = 500)
     private String description;
 
+    @NotNull
+    @Length(min = 1, max = 50)
     private String recommendation;
 
-    private  int ratings;
+    @NotNull
+    private  String ratings;
 
     private boolean watched;
 
