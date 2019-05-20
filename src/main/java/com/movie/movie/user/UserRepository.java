f6c68fee517dd39b693511ac83bc1051f56f9d9a
dengsang <dengsang@gmail.com>
2019-05-20 10:40:32 +0300

    Adding repository settings

--- src/main/java/com/movie/movie/user/UserRepository.java
+++ src/main/java/com/movie/movie/user/UserRepository.java
@@ -1,4 +1,11 @@
 package com.movie.movie.user;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByLogin(String username);
+    void deleteByLogin(String login);
 }
