6f0ede141d6844e2a588906f7ccfe51486eed453
dengsang <dengsang@gmail.com>
2019-05-20 11:00:22 +0300

    src/main/java/com/movie/movie/security/exception/JwtParseException.java

--- src/main/java/com/movie/movie/security/rest/AuthenticationController.java
+++ src/main/java/com/movie/movie/security/rest/AuthenticationController.java
@@ -1,4 +1,32 @@
 package com.movie.movie.security.rest;
 
+
+import lombok.RequiredArgsConstructor;
+import com.movie.movie.security.JwtGenerator;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequiredArgsConstructor
 public class AuthenticationController {
+
+    private final AuthenticationManager authenticationManager;
+    private final UserDetailsService userDetailsService;
+    private final JwtGenerator jwtGenerator;
+
+    @PostMapping("/api/auth")
+    public String createToken(@RequestBody JwtAuthenticationRequest authenticationRequest) {
+        Authentication authenticate = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword()));
+
+        UserDetails userDetails = userDetailsService.loadUserByUsername ( authenticationRequest.getUsername () );
+        return jwtGenerator.generate(userDetails);
+    }
 }
