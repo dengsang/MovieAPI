13805bcfe4586d9abd355928fea3c57cf1cef1af
dengsang <dengsang@gmail.com>
2019-05-20 10:48:16 +0300

    adding the user health check controller

--- src/main/java/com/movie/movie/system/health/HealthCheckController.java
+++ src/main/java/com/movie/movie/system/health/HealthCheckController.java
@@ -1,4 +1,19 @@
 package com.movie.movie.system.health;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+
 public class HealthCheckController {
+
+    @GetMapping("/api/health")
+    public Map<String, String> checkHealth() {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("status", "ok");
+        return map;
+    }
 }
