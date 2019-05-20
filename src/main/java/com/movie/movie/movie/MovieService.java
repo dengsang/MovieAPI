ffdc33cf36510ac9b68fee0c1b3910438d7b6b24
dengsang <dengsang@gmail.com>
2019-05-20 11:11:29 +0300

    adding movie service file

--- src/main/java/com/movie/movie/movie/MovieService.java
+++ src/main/java/com/movie/movie/movie/MovieService.java
@@ -1,9 +1,9 @@
-package com.movie.movie.MovieService;
+package com.movie.movie.movie;
 
+import com.movie.movie.user.User;
 import lombok.RequiredArgsConstructor;
 
 import com.movie.movie.system.exception.NotFoundException;
-import com.movie.movie.user.User;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,8 +14,6 @@ import static java.util.stream.Collectors.toList;
 
 @Service
 @RequiredArgsConstructor
-
-
 public class MovieService {
 
 
@@ -23,39 +21,45 @@ public class MovieService {
     private final MovieMapper movieMapper;
 
     @Transactional
-    public MovieDto create(MovieDto movieDto) {
-        Movie movie = movieMapper.map(movieDto);
+    public MovieData create(MovieData movieData) {
+        Movie movie = movieMapper.map( movieData );
         User user = new User();
-        user.setLogin(SecurityContextHolder.getContext().getAuthentication().getName());
+        user.setLogin( SecurityContextHolder.getContext().getAuthentication().getName());
         movie.setUser(user);
         movie = movieRepository.save(movie);
         return movieMapper.map(movie);
     }
 
-    public List<MovieDto> findAll() {
+    public List<MovieData> findAll() {
         return movieRepository.findAll().stream()
                 .map(movieMapper::map)
                 .collect(toList());
     }
 
-    public List<MovieDto> findWatched(boolean watched) {
+    public List<MovieData> findWatched(boolean watched) {
         return movieRepository.findByWatched(watched).stream()
                 .map(movieMapper::map)
                 .collect(toList());
     }
 
     @Transactional
-    public MovieDto update(String id, MovieDto dto) {
-        Movie movie = movieRepository.findById(id)
-                .orElseThrow(NotFoundException::new);
-        movie.setTitle(dto.getTitle());
-        movie.setDescription(dto.getDescription());
-        movie.setWatched(dto.isWatched());
+    public MovieData update(String id, MovieData data) {
+        Movie movie = movieRepository.findById(id).orElseThrow (NotFoundException::new );
+        //() -> new NotFoundException ()
+        //NotFoundException::new
+        movie.setTitle(data.getTitle());
+        movie.setDescription(data.getDescription());
+        movie.setRatings (data.getRatings ());
+        movie.setRecommendation ( data.getRecommendation ());
+        movie.setWatched(data.isWatched());
         return movieMapper.map(movie);
     }
 
     @Transactional
+    public void delete(String id, MovieData data) {
+        movieRepository.delete(data);
+    }
+
     public void delete(String id) {
-        movieRepository.delete(id);
     }
 }
