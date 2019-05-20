89d8523eedb55b78b5cc25cdf2c552009c18a2f8
dengsang <dengsang@gmail.com>
2019-05-20 10:45:50 +0300

    adding the user entity details under the user model

--- src/main/java/com/movie/movie/user/User.java
+++ src/main/java/com/movie/movie/user/User.java
@@ -1,4 +1,23 @@
 package com.movie.movie.user;
 
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "USER")
+
 public class User {
+
+    @Id
+    @Column(name = "LOGIN", length = 32)
+    private String login;
+
+    @Column(name = "PASSWORD", columnDefinition = "char(64)")
+    private String password;
 }
