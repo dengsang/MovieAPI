81d40aa5d2f6e9909013356f91fc121be1301754
dengsang <dengsang@gmail.com>
2019-05-20 10:44:48 +0300

    adding the user controller

--- src/main/java/com/movie/movie/user/UserController.java
+++ src/main/java/com/movie/movie/user/UserController.java
@@ -1,4 +1,18 @@
 package com.movie.movie.user;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+
 public class UserController {
+
+    private final UserService userService;
+
+    @DeleteMapping("/api/me")
+    public void removeAccount() {
+        userService.removeAccount();
+    }
 }
