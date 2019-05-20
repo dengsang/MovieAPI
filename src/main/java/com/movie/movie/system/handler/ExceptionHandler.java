a5e2bae49e795a189d32e006e3a970e7ec275431
dengsang <dengsang@gmail.com>
2019-05-20 10:52:33 +0300

    adding the exception file to handle use exceptions

--- src/main/java/com/movie/movie/system/handler/ExceptionHandler.java
+++ src/main/java/com/movie/movie/system/handler/ExceptionHandler.java
@@ -1,4 +1,16 @@
 package com.movie.movie.system.handler;
 
+
+import com.movie.movie.system.exception.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
 public class ExceptionHandler {
+
+    //@ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public void handleNotFoundException(){
+    }
 }
